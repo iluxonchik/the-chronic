8e0598e837ad7272ac457df962d6b24941eee353
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-12-31 21:55:45 +0000

    chore: init project

---
+++ tests/test_combinations.py
@@ -0,0 +1,12 @@
+"""
+Test if comibnations of words are working as expected.
+"""
+import unittest
+
+class CombinationsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass

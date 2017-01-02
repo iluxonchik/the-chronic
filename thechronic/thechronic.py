73c6792dd87536dce8bff6ff9cab4565722e0105
ILUXONCHIK <iluxon4ik@hotmail.com>
2017-01-02 23:57:13 +0000

    feat: implement loading words from files
    Related tests now pass

--- thechronic/thechronic.py
+++ thechronic/thechronic.py
@@ -2,15 +2,9 @@ from itertools import chain, product
 from thechronic.utils import is_iterable
 
 class TheChronic(object):
-    def __init__(self, words):
-        if is_iterable(words):
-            if is_iterable(words[1]):
-                # we have an iterable of iterables (ex: a list of lists)
-                self._words = list(chain.from_iterable(words))
-            else:
-                self._words = list(words)
-        else:
-            raise ValueError('Invalid argument for \'words\'.')
+    def __init__(self, words=[], files=[]):
+        self._parse_words_arg(words)
+        self._parse_files_arg(files)
 
     def combine(self, num_words=1):
         res = product(self._words, repeat=num_words)
@@ -20,3 +14,22 @@ class TheChronic(object):
     def _get_words_generator(self, words):
         for word_parts in words:
             yield ''.join(word_parts)
+
+    def _parse_words_arg(self, words):
+        if is_iterable(words):
+            if len(words) > 0 and is_iterable(words[0]):
+                # we have an iterable of iterables (ex: a list of lists)
+                self._words = list(chain.from_iterable(words))
+            else:
+                self._words = list(words)
+        else:
+            raise ValueError('Invalid argument for \'words\'.')
+
+    def _parse_files_arg(self, files):
+        if not is_iterable(files):
+            files = (files,)
+
+        for f in files:
+            with open(f, mode='r') as wfile:
+                file_words = wfile.read().splitlines()
+                self._words += file_words

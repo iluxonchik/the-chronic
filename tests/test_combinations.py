ae8d62a070606297cd579d652024c2f865bc832b
ILUXONCHIK <iluxon4ik@hotmail.com>
2017-01-02 23:56:42 +0000

    test: add tests for loading words from files

--- tests/test_combinations.py
+++ tests/test_combinations.py
@@ -12,6 +12,10 @@ class CombinationsTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.maxDiff = None
+        cls.TESTS_PATH = 'tests/'
+        cls.RES_PATH = cls.TESTS_PATH + 'res/'
+        cls.WORDS1_PATH = cls.RES_PATH + 'words1.txt'
+        cls.WORDS2_PATH = cls.RES_PATH + 'words2.txt'
 
     def setUp(self):
         self.words1 = ['cat', 'dog', 'rat']
@@ -80,7 +84,65 @@ class CombinationsTestCase(unittest.TestCase):
         pass
 
     def test_combinations_load_from_single_file(self):
-        pass
+        thechronic = TheChronic(files=self.WORDS1_PATH)
+
+        expected_result = [
+            'catcat', 'catdog', 'catrat',
+            'dogcat', 'dogdog', 'dograt',
+            'ratcat', 'ratdog', 'ratrat'
+        ]
+
+        res = list(thechronic.combine(num_words=2))
+        self.assertEqual(len(res), 9)
+        self.assertCountEqual(res, expected_result)
+
+        expected_result = get_words_generator(product(self.words1, repeat=3))
+        res = list(thechronic.combine(num_words=3))
+        self.assertEqual(len(res), 27)
+        self.assertCountEqual(res, expected_result)
 
     def test_combinations_load_from_multiple_file(self):
-        pass
+        thechronic = TheChronic(files=(self.WORDS1_PATH, self.WORDS2_PATH))
+
+        expected_result = ['catcat', 'catdog', 'catrat', 'catthe', 'catchronic',
+        'cat1992',
+        'dogcat', 'dogdog', 'dograt', 'dogthe', 'dogchronic', 'dog1992',
+        'ratcat', 'ratdog', 'ratrat', 'ratthe', 'ratchronic', 'rat1992',
+        'thecat', 'thedog', 'therat', 'thethe', 'thechronic', 'the1992',
+        'chroniccat', 'chronicdog', 'chronicrat', 'chronicthe',
+        'chronicchronic', 'chronic1992',
+        '1992cat', '1992rat', '1992dog', '1992the', '1992chronic', '19921992',
+        ]
+
+        res = list(thechronic.combine(num_words=2))
+        self.assertEqual(len(res), 36)
+        self.assertCountEqual(res, expected_result)
+
+        expected_result = get_words_generator(product(self.words1 + self.words2,
+            repeat=3))
+        res = list(thechronic.combine(num_words=3))
+        self.assertEqual(len(res), 216)
+        self.assertCountEqual(res, expected_result)
+
+    def test_combinaitons_load_from_words_and_file(self):
+        thechronic = TheChronic(words=self.words1, files=self.WORDS2_PATH)
+
+        expected_result = ['catcat', 'catdog', 'catrat', 'catthe', 'catchronic',
+        'cat1992',
+        'dogcat', 'dogdog', 'dograt', 'dogthe', 'dogchronic', 'dog1992',
+        'ratcat', 'ratdog', 'ratrat', 'ratthe', 'ratchronic', 'rat1992',
+        'thecat', 'thedog', 'therat', 'thethe', 'thechronic', 'the1992',
+        'chroniccat', 'chronicdog', 'chronicrat', 'chronicthe',
+        'chronicchronic', 'chronic1992',
+        '1992cat', '1992rat', '1992dog', '1992the', '1992chronic', '19921992',
+        ]
+
+        res = list(thechronic.combine(num_words=2))
+        self.assertEqual(len(res), 36)
+        self.assertCountEqual(res, expected_result)
+
+        expected_result = get_words_generator(product(self.words1 + self.words2,
+            repeat=3))
+        res = list(thechronic.combine(num_words=3))
+        self.assertEqual(len(res), 216)
+        self.assertCountEqual(res, expected_result)
